3a4453ebe9505b8a0f3f186e34a8ebecb4992d5b
Akteivias <77864295+Akteivias@users.noreply.github.com>
2024-05-21 16:32:20 -0700

    Update playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -20,12 +20,10 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? "100%" : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: process.env.CI
-    ? [['list', { printSteps: true }]]
-    : [
-        ['html', { open: 'on-failure' }],
-        ['list', { printSteps: true }],
-      ],
+  reporter: [
+    ['html', { open: 'on-failure' }],
+    ['list', { printSteps: true }],
+  ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
